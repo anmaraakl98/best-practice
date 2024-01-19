3b3ecb5bf39f2b410a8ecc3ee3a90a97c75392ef
anmaraakl98 <anmaraakl9@gmail.com>
2024-01-19 08:32:39 -0800

    Add AccountTybe Model And Migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function accountType()
+{
+  return $this->belongsTo(AccountType::class);
+}
 }
